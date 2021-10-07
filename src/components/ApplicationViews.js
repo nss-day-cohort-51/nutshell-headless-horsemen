0fa586cf312d902c35c31be2883987d8e1901c1d
Kaitlyn Long <katielong11@gmail.com>
2021-10-07 13:57:32 -0500

    created an input field for a new article and included a delete button

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Route } from "react-router-dom"
-import { ArticleCard } from "./Articles/ArticleCard"
 import { ArticleList } from "./Articles/ArticleList"
 import { FriendList } from "./Friends/FriendList"
+import { ArticleForm } from "./Articles/ArticleForm"
 
 export const ApplicationViews = () => {
   return (
@@ -11,17 +11,24 @@ export const ApplicationViews = () => {
       <Route exact path="/">
         <ArticleList/>
       </Route>
+
+      <Route path="/articles/create">
+          <ArticleForm />
+      </Route>
       
       <Route path="/friends">
         {/* Render the component for list of friends */}
         <FriendList />
       </Route>
+
       <Route path="/messages">
         {/* Render the component for the messages */}
       </Route>
+
       <Route path="/tasks">
         {/* Render the component for the user's tasks */}
       </Route>
+
       <Route path="/events">
         {/* Render the component for the user's events */}
       </Route>
