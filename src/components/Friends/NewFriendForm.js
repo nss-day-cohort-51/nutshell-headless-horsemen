12560a4fc8f303fefb6a90c30de97a2e9f2b69d3
Devery Page <pagedevery@gmail.com>
2021-10-08 09:24:44 -0500

    trying to get friends form to display

--- src/components/Friends/NewFriendForm.js
+++ src/components/Friends/NewFriendForm.js
@@ -1,22 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { addFriend, getAllFriends } from "./FriendManager";
 import './NewFriendForm.css'
 
 export const NewFriendForm = () => {
 
-    
-	const [friend, setFriend] = useState({
-		id: 0,
-		userId:0,
-		currentUserId:0
-	});
+
+    const [friend, setFriend] = useState({
+        id: 0,
+        userId: 0,
+        currentUserId: 0
+    });
 
     const [isLoading, setIsLoading] = useState(false);
 
     const [friends, setFriends] = useState([])
     const history = useHistory;
-    
+
     const handleControlledInputChange = (event) => {
         /* When changing a state object or array,
         always create a copy, make changes, and then set state.*/
@@ -33,36 +33,39 @@ export const NewFriendForm = () => {
         // update state
         setFriend(newFriend)
     }
-    
-    useEffect (() => {
+
+    useEffect(() => {
         getAllFriends().then(friends => {
             setFriend(friends)
         })
     }, []);
-    
+
     const handleClickSaveFriend = (event) => {
         event.preventDefault()
-        
+
         const friendId = friend.friendId
-        
+
         if (friendId === 0) {
             window.alert("Please select a friend")
         } else {
             addFriend(friend)
-            .then(() => history.push("/friends"))
+                .then(() => history.push("/friends"))
         }
     }
-    
+
     return (
         <form className="friendForm">
-			<h2 className="friendForm__title">New Friend</h2>
-			<fieldset>
-				<div className="form-group">
-					<label htmlFor="name">UserName:</label>
-					<input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Username" value={friend.name} />
-				</div>
-			</fieldset>
-            </form>
+            <h2 className="friendForm__title">New Friend</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">UserName:</label>
+                    <input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Username" value={friend.name} />
+                </div>
+                <button className="btn btn-primary"
+                    onClick={handleClickSaveFriend}>
+                    Add Friend
+                </button>
+            </fieldset>
+        </form>
     )
 }
-    
