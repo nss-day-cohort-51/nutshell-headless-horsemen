ddb6e81efb2a654012adf4574ac13ebe0021bafb
Devery Page <pagedevery@gmail.com>
2021-10-12 10:41:40 -0500

    message card, List, and manager updated

--- src/components/Messages/MessageCard.js
+++ src/components/Messages/MessageCard.js
@@ -1,1 +1,15 @@
-placeHolder
+import React from "react";
+import "./Message.css";
+
+export const MessageCard = ({message, handleDeleteMessage}) => {
+
+    return (
+        <div className="message_card">
+        <div className="message_card_content">
+        <h4>{message.userId}</h4>
+        <p>{message.message}</p>
+        <button type="button-delete" onClick={() => handleDeleteMessage(message.id)}>Delete</button>
+        </div>
+        </div>
+    )
+}
