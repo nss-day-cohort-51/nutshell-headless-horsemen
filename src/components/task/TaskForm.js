415bb38c306df19fd3cdb3e394e9cbf226741659
Ben Mosley <bmosley1132@gmail.com>
2021-10-12 17:01:22 -0400

    made some comments for clarification

--- src/components/task/TaskForm.js
+++ src/components/task/TaskForm.js
@@ -1,8 +1,9 @@
+// Worked on by Ben
+
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { addTask } from './TaskManager';
 import './TaskForm.css';
-
 
 export const TaskForm = () => {
 	const [task, setTask] = useState({
@@ -11,21 +12,23 @@ export const TaskForm = () => {
 		completeBy: "",
 		userId: 1
 	});
+// shows user data is being loaded but is not fully loaded yet
     const [isLoading, setIsLoading] = useState(false);
-
+// all the URL visited on DOM
 	const history = useHistory();
-
+//allows for inputting the info for a task
    const handleFieldChange = event => {
     const stateToChange = { ...task };
     stateToChange[event.target.id] = event.target.value;
     setTask(stateToChange);
 };
 
-
+//adds new task to DOM and prevents HTML from displaying to DOM
 const constructNewTask = event => {
     event.preventDefault();
     addTask(task)
     .then(() => history.push("/tasks"))
+//prevents edited task from displaying unless all input fields have something in them
     if (task.name === "" || task.completeBy === 0) {
         window.alert("Please complete all fields");
     } else {
@@ -34,9 +37,6 @@ const constructNewTask = event => {
     
     }
 };
-
-// const activeId = sessionStorage.getItem("activeUser")
-// task.userId = parseInt(activeId)
 
 return (
     <>
